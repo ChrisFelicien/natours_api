05d98e5b3ae99a2fcf346842725d37ecdb0282b3
Chris felicien <f.ilungakalenga@gmail.com>
2024-11-25 02:33:44 +0200

    Add the hpp for whitelist duplicate list

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 import express from "express";
+import helmet from "helmet";
 import rateLimit from "express-rate-limit";
+import mongoSanitize from "express-mongo-sanitize";
+import hpp from "hpp";
+import xss from "xss-clean";
 import ErrorConstructor from "./utils/Errors.js";
 import globalError from "./controllers/globalErrorHandler.js";
 import toursRoutes from "./routes/toursRoute.js";
@@ -8,15 +12,25 @@ import morgan from "morgan";
 
 const app = express();
 
+app.use(helmet());
 if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
-app.use(express.json());
-// Define routes
+
+app.use(express.json({ limit: "10kb" }));
+
+app.use(mongoSanitize());
+app.use(xss());
 
 const limiter = rateLimit({
   max: 300,
   windowMs: 60 * 60 * 1000,
   message: "Too many request please try again later."
 });
+
+app.use(
+  hpp({
+    whitelist: ["duration"]
+  })
+);
 
 app.use("/api", limiter);
 app.use("/api/v1/tours", toursRoutes);
